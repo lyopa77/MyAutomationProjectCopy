b287a55a02cf8cb0288fd2fbc6ed650b7de01216
lyopa <lyopa@hotmail.com>
2020-12-23 21:16:50 -0500

    modified again

--- src/test/java/com/myapp/runners/CukeRunner.java
+++ src/test/java/com/myapp/runners/CukeRunner.java
@@ -3,13 +3,13 @@ package com.myapp.runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-
+//try again
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"json:target/cucumber.json"},
         features = "src/test/resources/features",
-        glue = "com/myapp/step_definitions",
-        dryRun = false
+        glue = "com/myapp/step_definitions"
+       // dryRun = false
 )
 public class CukeRunner {
 
